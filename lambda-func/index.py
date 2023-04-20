9729f97e63491c6485bb2738f15407d068e688fd
kHerashchenko <kategera6@gmail.com>
2023-04-20 18:56:05 +0200

    youtube video handler added

--- lambda-func/index.py
+++ lambda-func/index.py
@@ -1,13 +1,13 @@
 import json
 import telebot
 import openai
+import botocore.session
+from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
+from youtube_video_handler import generate_transcript, retrieve_metadata
 
 tg_bot_token_secret_name = "TelegramBotToken"
 openai_key_secret_name = "OpenAISecretKey"
 region_name = "eu-north-1"
-
-import botocore.session
-from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
 
 client = botocore.session.get_session().create_client('secretsmanager')
 cache_config = SecretCacheConfig()
@@ -44,8 +44,32 @@ def send_welcome(message):
                   "I am here to burn your free tier credits."))
 
 
+# Handle '/get_transcript'
+@bot.message_handler(commands=['get_transcript'])
+def send_transcript(message):
+    bot.reply_to(message, "Please provide youtube video link")
+    print('here 2')
+    bot.register_next_step_handler_by_chat_id(message.chat.id, process_youtube_video_link)
+    print('here 3')
+
+
+def process_youtube_video_link(message):
+    print('here 1')
+    url = message.text
+    try:
+        title, author = retrieve_metadata(url)
+    except:
+        bot.reply_to(message, "Invalid youtube video link")
+        return
+
+    transcript, no_of_words, filename = generate_transcript(url)
+    doc = open(filename, 'rb')
+    bot.send_message(message.chat.id, f"Title: {title}\nAuthor: {author}\nVideo transcript:")
+    bot.send_document(message.chat.id, doc)
+
+
 # Handle all other messages
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
+def send_response_from_openapi(message):
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": message.text}])
     bot.reply_to(message, response.choices[0].message.content)
