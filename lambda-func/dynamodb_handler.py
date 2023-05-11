46569504c442aa38f76e703da2d630888b0beeed
kHerashchenko <kategera6@gmail.com>
2023-05-11 15:51:29 +0200

    openai user secrets handling, list user videos command

--- lambda-func/dynamodb_handler.py
+++ lambda-func/dynamodb_handler.py
@@ -3,15 +3,79 @@ import os
 users_table = os.environ['USERS_NAME']
 videos_table = os.environ['VIDEOS_NAME']
 
-def add_video_to_user(client, chat_id, video_id):
-    """Adds video_id into videos list if it is not there yet."""
-    videos_list = client.get_item(
+
+def retrieve_user_videos(client, chat_id):
+    videos_resp = client.get_item(
         TableName=users_table,
         Key={
-            'chat_id': chat_id,
+            'chat_id': {'N': str(chat_id)},
         },
         AttributesToGet=['videos']
-    )['videos']
+    )
+    if not videos_resp['Item']:
+        videos_list = []
+    else:
+        videos_list = videos_resp['Item']['videos']
+
+    print(videos_list)
+    user_videos = []
+    for vid in videos_list['L']:
+        vid_id = vid['S']
+        vid_info_resp = client.get_item(
+            TableName=videos_table,
+            Key={
+                'video_id': {'S': str(vid_id)},
+            }
+        )
+        vid_info = {}
+        for attr, val_dict in vid_info_resp['Item'].items():
+            vid_info[attr] = list(val_dict.values())[0]
+        user_videos.append(vid_info)
+
+    return user_videos
+
+
+def retrieve_user_openai_creds(client, chat_id):
+    openai_key_resp = client.get_item(
+        TableName=users_table,
+        Key={
+            'chat_id': {'N': str(chat_id)},
+        },
+        AttributesToGet=['openai_key']
+    )
+    if not openai_key_resp['Item']:
+        return None
+    else:
+        return openai_key_resp['Item']['openai_key']['S']
+
+
+def add_video_to_user(client, chat_id, video_id):
+    """Adds video_id into videos list if it is not there yet."""
+    videos_resp = client.get_item(
+        TableName=users_table,
+        Key={
+            'chat_id': {'N': str(chat_id)},
+        },
+        AttributesToGet=['videos']
+    )
+    if not videos_resp['Item']:
+        data = client.update_item(
+            TableName=users_table,
+            Key={
+                'chat_id': {'N': str(chat_id)},
+            },
+            UpdateExpression = f"SET videos = :i",
+            ExpressionAttributeValues={
+                ':i': {'L': []},
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        print(data)
+
+        videos_list = []
+    else:
+        videos_list = videos_resp['Item']['videos']
+
 
     if video_id not in videos_list:
         data = client.update_item(
@@ -21,7 +85,7 @@ def add_video_to_user(client, chat_id, video_id):
             },
             UpdateExpression="SET videos = list_append(videos, :i)",
             ExpressionAttributeValues={
-                ':i': [video_id],
+                ':i': {'L': [{'S': video_id}]}
             },
             ReturnValues="UPDATED_NEW"
         )
@@ -49,11 +113,13 @@ def update_user_item(client, chat_id, **kwargs):
 
 
 def update_video_item(client, video_id, **kwargs):
+
     for key, value in kwargs.items():
+        print(key, value)
         data = client.update_item(
             TableName=videos_table,
             Key={
-                'video_id': {'N': str(video_id)},
+                'video_id': {'S': str(video_id)},
             },
             UpdateExpression = f"SET {key} = :i",
             ExpressionAttributeValues={
@@ -64,3 +130,4 @@ def update_video_item(client, video_id, **kwargs):
         print(data)
 
     return 1
+
