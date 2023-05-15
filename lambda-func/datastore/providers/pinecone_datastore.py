5ca5d09a352c524a136f08699fbc842a61488dd1
Wortes02 <zjhblx@inf.elte.hu>
2023-05-15 15:02:03 +0200

    Pinecone bug fixed

--- lambda-func/datastore/providers/pinecone_datastore.py
+++ lambda-func/datastore/providers/pinecone_datastore.py
@@ -16,9 +16,13 @@ from models.models import (
 from services.date import to_unix_timestamp
 
 # Read environment variables for Pinecone configuration
-PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
-PINECONE_ENVIRONMENT = os.environ.get("PINECONE_ENVIRONMENT")
-PINECONE_INDEX = os.environ.get("PINECONE_INDEX")
+PINECONE_API_KEY="f6a4c25b-43c5-4016-8f34-6647c56fc5c3"
+PINECONE_ENVIRONMENT="northamerica-northeast1-gcp"
+PINECONE_INDEX="gpt-test"
+
+# PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+# PINECONE_ENVIRONMENT = os.environ.get("PINECONE_ENVIRONMENT")
+# PINECONE_INDEX = os.environ.get("PINECONE_INDEX")
 assert PINECONE_API_KEY is not None
 assert PINECONE_ENVIRONMENT is not None
 assert PINECONE_INDEX is not None
